4d656013fb7abcb6c672a11106514fab746d4066
jsl5 <jsl5@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-29 17:40:17 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/daewr@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mixexp/R/Xvert.R
+++ pkg/mixexp/R/Xvert.R
@@ -1,14 +1,17 @@
-Xvert = function(x1=c(0,1),x2=c(0,1),x3=c(0,1),x4=c(0,1),x5=c(0,1),x6=c(0,1),
+Xvert = function(nfac=3,x1=c(0,1),x2=c(0,1),x3=c(0,1),x4=c(0,1),x5=c(0,1),x6=c(0,1),
                  x7=c(0,1),x8=c(0,1),x9=c(0,1),x10=c(0,1),x11=c(0,1),x12=c(0,1),ndm=0)
 {
 # Create the constraints matrix for crvtave
 ck<-cbind(x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12)
 for (i in 1:12){
   cks<-ck[1,i]+(1-ck[2,i])
-  if (cks!=0) {
-    nfac=i 
+   if (cks!=0) {
+    nfacc=i 
      } else {break}
  }
+if (nfacc>nfac) {
+         stop(" The number of upper and lower limits supplied exceeds the number of factors")
+                }
 v<-c(-x1[1],x1[2])
 for (i in 2:nfac) {
  v<-c(v,-ck[1,i],ck[2,i])
@@ -18,6 +21,8 @@ Ip<-diag(nfac)
 In<--1*Ip
 conmx<-interleave(Ip,In)
 conmx<-cbind(conmx,v)
+
+
 # calls crvtave to create exteme vertices design plus centroid
 des<-crvtave(ndm,conmx)
 des<-data.frame(des)
@@ -28,3 +33,4 @@ DesignPoints(des=des,x1lower=-v[1], x1upper=v[2],x2lower=-v[3],x2upper=v[4],x3lo
  }
 return(des)
 }
+
