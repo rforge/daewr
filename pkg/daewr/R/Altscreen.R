7d0d1a98bef2cc82b92a1caa74e26afc82bc03ba
jsl5 <jsl5@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-25 23:57:01 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/daewr@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/daewr/R/Altscreen.R
+++ pkg/daewr/R/Altscreen.R
@@ -1,6 +1,5 @@
 Altscreen<-function(nfac) {
 library(daewr)
-#D16F6 <- D16F7 <- D16F8 <- NULL # sets variables to null value first 
  if (nfac<6) {stop("Alternate screening designs exist only for 6, 7 or 8 factors") }
  if (nfac>8) {stop("Alternate screening designs exist only for 6, 7 or 8 factors") }
  if (nfac==6) {data(D16F6)
