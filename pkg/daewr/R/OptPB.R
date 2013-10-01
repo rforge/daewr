db66777b039e0b02bc1ace25402d078b38b205eb
jsl5 <jsl5@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-01 15:07:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/daewr@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/daewr/R/OptPB.R
+++ pkg/daewr/R/OptPB.R
@@ -1,6 +1,5 @@
 OptPB<-function(nruns,nfactors, randomize=FALSE){
   if (nruns != 12 & nruns !=20 ) {stop("This function only works for nruns=12 or nruns=20")}
-library(FrF2)
 if (nruns==12) {
   if (nfactors < 5) {stop("At least 5 factors required for 12-run PB design")}
   if (nfactors==5 | nfactors==6 | nfactors==7 | nfactors==8) { design<-pb(12,nfactors)
